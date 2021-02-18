87768e12246cd2c0a9ec4169606886883f1ba199
tarek doha <tarikdoha80@gmail.com>
2021-02-18 20:29:05 +0100

    update contacts

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,33 +1,36 @@
-export const getStaticPaths= async()=>{
+export const getStaticPaths = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/users")
-    const data= await res.json();
-    
-    const paths=data.map(ninja=>{
-        return{
-            params:{id: ninja.id.toString()}
+    const data = await res.json();
+
+    const paths = data.map(ninja => {
+        return {
+            params: { id: ninja.id.toString() }
         }
     })
 
-    return{
+    return {
         paths,
-        fallback:false
+        fallback: false
     }
 }
 
-export const getStaticProps= async(context)=>{
-    const id=context.params.id
-    const res= await fetch("https://jsonplaceholder.typicode.com/users/"+id);
-    const data=await res.json();
-    return{
-        props:{ninja:data}
-}
+export const getStaticProps = async (context) => {
+    const id = context.params.id
+    const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+    const data = await res.json();
+    return {
+        props: { ninja: data }
+    }
 }
 
 
-const Details = ({ninja}) => {
-    return ( <div>
-        <h1>{ninja.name}</h1>
-    </div> );
+const Details = ({ ninja }) => {
+    return (<div>
+        <h1>Contact Info</h1>
+        <h2>Name: {ninja.name}</h2>
+        <h2>Email: {ninja.email}</h2>
+        <h2>Company: {ninja.company.name}</h2>
+    </div>);
 }
- 
+
 export default Details;
